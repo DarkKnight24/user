cdf0828233760d3f1ac5b44b7b258a8f2251f901
huahua <17638351213@163.com>
2020-04-22 10:06:30 +0800

    用户登录空指针异常

--- src/main/java/com/movie/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/movie/user/service/impl/UserServiceImpl.java
@@ -1,8 +1,17 @@
 package com.movie.user.service.impl;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.movie.base.dto.UserBaseDto;
 import com.movie.base.utils.BeanUtil;
 import com.movie.base.utils.MD5Utils;
+import com.movie.file.utils.Utils;
 import com.movie.user.dao.UserMapper;
 import com.movie.user.dto.UserCinemaDto;
 import com.movie.user.dto.UserCinemaReleationDto;
@@ -10,50 +19,43 @@ import com.movie.user.dto.UserLoginDto;
 import com.movie.user.entity.User;
 import com.movie.user.service.UserCinemaReleationService;
 import com.movie.user.service.UserService;
-import com.movie.file.utils.Utils;
-import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class UserServiceImpl implements UserService {
-
+    
     @Resource
     private UserMapper userMapper;
-
+    
     @Autowired
     private Utils utils;
-
+    
     @Autowired
     private UserCinemaReleationService userCinemaReleationService;
-
+    
     @Override
     public int deleteByPrimaryKey(Long userId) {
         return userMapper.deleteByPrimaryKey(userId);
     }
-
+    
     @Override
     public int insert(User record) {
         record.setUserPwd(MD5Utils.MD5(record.getUserName() + record.getUserPwd()));
         int i;
-        try{
+        try {
             i = userMapper.insert(record);
-        }catch (Exception e){
+        }
+        catch (Exception e) {
             return 0;
         }
         return i;
     }
-
+    
     @Override
     public int insertSelective(User record) {
         return userMapper.insertSelective(record);
     }
-
+    
     @Override
     public UserBaseDto selectByPrimaryKey(Long userId) {
         UserBaseDto userBaseDto = new UserBaseDto();
@@ -63,36 +65,40 @@ public class UserServiceImpl implements UserService {
         }
         return userBaseDto;
     }
-
+    
     @Override
     public int updateByPrimaryKeySelective(User record) {
         return userMapper.updateByPrimaryKeySelective(record);
     }
-
+    
     @Override
     public User updateByPrimaryKey(User record) {
         int i = userMapper.updateByPrimaryKeySelective(record);
         record.setUserHeadImg(utils.getFileUrl(record.getUserHeadImg()));
-        return i>0?record:null;
+        return i > 0 ? record : null;
     }
-
+    
     @Override
     public int updateBatchSelective(List<User> list) {
         return userMapper.updateBatchSelective(list);
     }
-
+    
     @Override
     public Object login(UserLoginDto userLoginDto) {
         User user = userMapper.selectByUserName(userLoginDto.getUserName());
+        if (user == null) {
+            return false;
+        }
         if (MD5Utils.MD5(userLoginDto.getUserName() + userLoginDto.getUserPwd()).equals(user.getUserPwd())) {
             UserBaseDto userBaseDto = new UserBaseDto();
-            BeanUtil.copyProperties(user,userBaseDto);
+            BeanUtil.copyProperties(user, userBaseDto);
             userBaseDto.setUserHeadImg(utils.getFileUrl(userBaseDto.getUserHeadImg()));
             UserCinemaDto userCinemaDto = new UserCinemaDto();
-            BeanUtil.copyProperties(userBaseDto,userCinemaDto);
-            if(userBaseDto.getUserRole()==2){
-                UserCinemaReleationDto userCinemaReleationDto = userCinemaReleationService.selectByUserId(userBaseDto.getUserId());
-                BeanUtil.copyProperties(userCinemaReleationDto,userCinemaDto);
+            BeanUtil.copyProperties(userBaseDto, userCinemaDto);
+            if (userBaseDto.getUserRole() == 2) {
+                UserCinemaReleationDto userCinemaReleationDto =
+                    userCinemaReleationService.selectByUserId(userBaseDto.getUserId());
+                BeanUtil.copyProperties(userCinemaReleationDto, userCinemaDto);
             }
             return userCinemaDto;
         }
